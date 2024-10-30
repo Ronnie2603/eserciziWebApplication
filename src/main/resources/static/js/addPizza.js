10df4785270d427e909c8329a4f1450291baaa6f
Ronnie2603 <verdeb26@gmail.com>
2024-10-30 09:11:29 +0000

    Inserita validazione dei form tramite JavaScript

--- src/main/resources/static/js/addPizza.js
+++ src/main/resources/static/js/addPizza.js
@@ -1,11 +1,22 @@
-function addPizza() {
-      const table = document.getElementById("pizza-table").getElementsByTagName('tbody')[0];
+document.getElementById("addPizzaForm").addEventListener("submit", function(event) {
+      event.preventDefault();
+
       const name = document.getElementById("pizzaName").value;
       const ingredients = document.getElementById("pizzaIngredients").value;
       const price = document.getElementById("pizzaPrice").value;
 
-      const row = table.insertRow();
-      row.innerHTML = `<td>${name}</td><td>${ingredients}</td><td>€${price}</td><td><button class="btn btn-danger" onclick="removePizza(this)">Elimina</button></td>`;
-
+      const table = document.getElementById("pizza-table").getElementsByTagName('tbody')[0];
+  
+      addPizza(table, name, ingredients, price);
+  
       document.getElementById("addPizzaForm").reset();
+});
+
+
+function addPizza(table, name, ingredients, price) {
+      const row = table.insertRow();
+      row.innerHTML = `<td>${name}</td>
+                       <td>${ingredients}</td>
+                       <td>€${price}</td>
+                       <td><button class="btn btn-danger" onclick="removePizza(this)">Elimina</button></td>`;
 }
